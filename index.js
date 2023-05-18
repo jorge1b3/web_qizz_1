775ea83943012484efa4a6a5a11ef73f843a7944
jorge1b3 <locodeira1b3@gmail.com>
2023-05-18 01:33:55 -0500

    cambiando lógica para usar ciertos campos

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// const apiId = '4fzu21pdr0p0xars8tx432sza'
-// const apiKey = '3znq8gzgg2h7229yo1mlepjmf0dy2vk8q4t1reeq33gov8rewd';
 const api_url = 'https://www.datos.gov.co/resource/ccvq-rp9s.json';
-
 const responseField = document.querySelector('#table');
 
 async function getData(departamento){
@@ -16,43 +13,54 @@ async function getData(departamento){
 
 async function genTable(event) {
     event.preventDefault();
-    const departamento = document.getElementById("input").value;
+    const departamento = document.getElementById("input").value.toUpperCase();
+
+    const spinner = document.createElement("div");
+    spinner.className = "spinner-border align-content-lg-center";
+    responseField.innerHTML = spinner.outerHTML;
 
     const data = await getData(departamento).catch((error) =>{
         console.log(error);
         throw new Error("No se pudo procesar la solicitud");
     })
-
-    const tabla   = document.createElement("table");
+    const table   = document.createElement("table");
     const tblHead = document.createElement("thead");
+    const firstRow = document.createElement("tr");
     const tblBody = document.createElement("tbody");
-    tabla.className= "table table-bordered table-striped table-sm"
+    table.className= "table table-striped table-dark"
 
-    let tbltr = document.createElement("tr");
-    for (head in data[0]){
-        const textoCelda = document.createTextNode(`${head}`)
-        const celda = document.createElement("th");
-        celda.className = "col text-center"
-        celda.appendChild(textoCelda);
-        tbltr.appendChild(celda);
+    const fields = {
+        "Fecha" : "fechaobservacion",
+        "Valor" : "valorobservado",
+        "Estación" : "nombreestacion",
+        "Departamento" : "departamento",
+        "Municipio" : "municipio"
     }
-    tblHead.appendChild(tbltr);
-    tabla.appendChild(tblHead);
 
-    for (const row of data.slice(0,10)){
-        const hilera = document.createElement("tr");
-        hilera.className = "text-sm-center text-wrap"
-        for (value in row){
-            const celda = document.createElement("td");
-            const textoCelda = document.createTextNode(`${row[value]}`);
-            celda.appendChild(textoCelda);
-            hilera.appendChild(celda);
+    for (const field in fields){
+        const cellText = document.createTextNode(`${field}`)
+        const cell = document.createElement("th");
+        cell.className = "col text-center"
+        cell.appendChild(cellText);
+        firstRow.appendChild(cell);
+    }
+    tblHead.appendChild(firstRow);
+    table.appendChild(tblHead);
+
+    for (const dataRow of data.slice(0,10)){
+        const row = document.createElement("tr");
+        row.className = "text-sm-center text-wrap"
+        for (const field in fields){
+            const cell = document.createElement("td");
+            const cellText = document.createTextNode(`${dataRow[fields[field]]}`);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
         }
-        tblBody.appendChild(hilera);
+        tblBody.appendChild(row);
     }
     
-    tabla.appendChild(tblBody);
-    responseField.innerHTML = tabla.outerHTML;
+    table.appendChild(tblBody);
+    setTimeout(() => responseField.innerHTML = table.outerHTML, 500);
 }
 
 document.getElementById("form").addEventListener("submit",genTable);
