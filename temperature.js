833043cbae808f63e4d4f811475bf407927dc730
jorge1b3 <locodeira1b3@gmail.com>
2023-05-19 18:25:02 -0500

    Apliando recomendaciones y ";"

--- temperature.js
+++ temperature.js
@@ -1,59 +1,59 @@
-const apiUrl = 'https://www.datos.gov.co/resource/ccvq-rp9s.json'
+const apiUrl = 'https://www.datos.gov.co/resource/ccvq-rp9s.json';
 
-const form = document.querySelector('#form')
-const responseField = document.querySelector('#table')
-const inputField = document.querySelector('#input')
+const form = document.querySelector('#form');
+const responseField = document.querySelector('#table');
+const inputField = document.querySelector('#input');
 
 const getData = async () => {
-  const departamento = inputField.value.toUpperCase()
-  const url = departamento.length === 0 ? apiUrl : apiUrl.concat('?departamento=', departamento)
+  const departamento = inputField.value.toUpperCase();
+  const url = departamento.length === 0 ? apiUrl : apiUrl.concat('?departamento=', departamento);
   try {
-    const response = await fetch(url)
+    const response = await fetch(url);
     if (response.ok) {
-      const jsonResponse = await response.json()
-      renderResponse(jsonResponse)
-    }
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    };
   } catch (e) {
-    console.log(e)
-  }
-}
+    console.log(e);
+  };
+};
 
 const generateTable = event => {
-  event.preventDefault()
+  event.preventDefault();
 
   // Limpiamos el contenedor de la tabla
   while (responseField.firstChild) {
-    responseField.removeChild(responseField.firstChild)
-  }
+    responseField.removeChild(responseField.firstChild);
+  };
 
   // Dibujamos un spiner
-  const spinnerPos = document.createElement('div')
-  spinnerPos.className = 'd-flex justify-content-center'
-  const spinner = document.createElement('div')
-  spinner.className = 'spinner-border'
-  spinnerPos.appendChild(spinner)
-  responseField.appendChild(spinnerPos)
+  const spinnerPos = document.createElement('div');
+  spinnerPos.className = 'd-flex justify-content-center';
+  const spinner = document.createElement('div');
+  spinner.className = 'spinner-border';
+  spinnerPos.appendChild(spinner);
+  responseField.appendChild(spinnerPos);
 
-  getData()
-}
+  getData();
+};
 
 const renderResponse = data => {
   if (data.errors) {
-    responseField.textContent = 'No se ha podido generar la tabla'
-    return
-  }
+    responseField.textContent = 'No se ha podido generar la tabla';
+    return;
+  };
 
   // Limpiamos el contenedor de la tabla
   while (responseField.firstChild) {
-    responseField.removeChild(responseField.firstChild)
-  }
+    responseField.removeChild(responseField.firstChild);
+  };
 
   // Creando partes de la tabla
-  const table = document.createElement('table')
-  const tblHead = document.createElement('thead')
-  const firstRow = document.createElement('tr')
-  const tblBody = document.createElement('tbody')
-  table.className = 'table table-striped table-blue text-center text-wrap'
+  const table = document.createElement('table');
+  const tblHead = document.createElement('thead');
+  const firstRow = document.createElement('tr');
+  const tblBody = document.createElement('tbody');
+  table.className = 'table table-striped table-blue text-center text-wrap';
 
   // Los campos a buscar y mostrar
   const fields = new Map([
@@ -62,37 +62,37 @@ const renderResponse = data => {
     ['Estación', 'nombreestacion'],
     ['Departamento', 'departamento'],
     ['Municipio', 'municipio']
-  ])
+  ]);
 
   // Llenando cabecera
   Array.from(fields.keys()).forEach(label => {
-    const cell = document.createElement('th')
-    cell.className = 'col'
-    cell.textContent = label
-    firstRow.appendChild(cell)
-  })
+    const cell = document.createElement('th');
+    cell.className = 'col';
+    cell.textContent = label;
+    firstRow.appendChild(cell);
+  });
 
   // Guardamos la cabecera en la tabla
-  tblHead.appendChild(firstRow)
-  table.appendChild(tblHead)
+  tblHead.appendChild(firstRow);
+  table.appendChild(tblHead);
 
   // Llendando cuerpo de la tabla
   data.slice(0, 10).forEach(currentItem => {
-    const row = document.createElement('tr')
-    row.className = 'shadow-sm'
+    const row = document.createElement('tr');
+    row.className = 'shadow-sm';
     Array.from(fields.values()).forEach(value => {
-      const cell = document.createElement('td')
-      cell.textContent = currentItem[value]
-      row.appendChild(cell)
-    })
-    tblBody.appendChild(row)
-  })
+      const cell = document.createElement('td');
+      cell.textContent = currentItem[value];
+      row.appendChild(cell);
+    });
+    tblBody.appendChild(row);
+  });
 
   // Guardamos el body en la tabla
-  table.appendChild(tblBody)
+  table.appendChild(tblBody);
   // Agregamos la tabla al DOM
-  responseField.appendChild(table)
-}
+  responseField.appendChild(table);
+};
 
 // Agregamos un evento al botón form para que ejecutr generateTable al hacer click
-form.addEventListener('submit', generateTable)
+form.addEventListener('submit', generateTable);
